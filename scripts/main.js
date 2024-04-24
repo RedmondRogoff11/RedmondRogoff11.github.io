21e7d792b061a1771a8b7e27181d85d5d4b93fd6
RedmondRogoff11 <144718607+RedmondRogoff11@users.noreply.github.com>
2024-04-24 18:21:43 +0000

    task 1

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+console.log("Task 1");
+for(let i = 1; i < 11; i++) {
+    console.log (i +1);
+}
+console.log("Task 2");
+
+console.log("Task 3");
+
+console.log("Task 4");
+let sum = 0;
+console.log("Task 5");
