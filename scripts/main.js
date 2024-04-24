439522890659c5dd72cef5c9607fdc8885cba2e4
RedmondRogoff11 <144718607+RedmondRogoff11@users.noreply.github.com>
2024-04-24 18:27:46 +0000

    task 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 console.log("Task 1");
 for(let i = 1; i < 11; i++) {
-    console.log (i +1);
+    console.log (i * i);
 }
 console.log("Task 2");
-
+for(let i = 5; i > 0; i--)(
+    console.log(i)
+)
 console.log("Task 3");
 
 console.log("Task 4");
