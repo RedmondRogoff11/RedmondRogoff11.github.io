9240c70a2789ca91eaff9fbbbbbdf50312b16c1b
RedmondRogoff11 <144718607+RedmondRogoff11@users.noreply.github.com>
2024-06-05 21:47:36 +0000

    save

--- scripts/main.js
+++ scripts/main.js
@@ -1,67 +1,7 @@
-console.log("Task 1");
-for(let i = 1; i < 11; i++) {
-    console.log (i * i);
-}
-console.log("Task 2");
-for(let i = 5; i > 0; i--)(
-    console.log(i)
-)
-console.log("blastoff")
-
-console.log("Task 3");
-for(let i = 0; i < 51; i+=2){
-    console.log(i);
-}
-
-console.log("Task 4");
-let sum = 0;
-
-for(let i = 0; i < 101; i++) {
-    sum+= i;
-} 
-console.log(sum);
-//TASK 5 COMPLETE (BELOW)+++++
-console.log("Task 5");
-let factorial = 1;
-for (let i = 1; i <= 10; i++) {
-    factorial *= i;
-    console.log(factorial);
-}
-console.log("Task 6");
-let arr = [3,1,4,1,5];
-for(let i = 0; i < 5; i++ ) {
-    console.log(arr[i]);
-};
-console.log("Task 7");
-for(let i = 0; i <arr.length; i++) {
-    console.log(arr.at(-i-1));
-}
-console.log("Task 8");
-let cubes = [];
-for(let i = 1; i < 11; i++ ) {
-    cubes.push (i**3)
-    }
-    console.log(cubes)
-console.log("Task 9");
-let fib = [];
-let first = 0;
-let second = 1;
-for(let i =1; i <11; i++) {
-
-fib .push (first)
-let temp = second
-second = second + first;
-first = temp
-}
-
-console.log(fib);
-
-console.log("Task 10");
-
-let newarray = [];
-
-for(let i = 0; i < arr.length; i++) {
-    newarray.push(arr.at(-i-1))
-}
-console.log(newarray);
-
+let list = document.getElementById("playerlist");
+let inputBox = document.getElementById("inputBox");
+document.getElementById("button").addEventListener("click", function () {
+    let li = document.createElement('li');
+     li.innerText = inputBox.value;
+    list.appendChild(li);
+}); 
